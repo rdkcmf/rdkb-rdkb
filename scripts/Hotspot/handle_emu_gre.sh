eb8ae97282b1ca18c3a6327b9b17b53408857918
Sachin Kitnawat <Sachin_Kitnawat@comcast.com>
2018-09-25 09:52:58 +0000

    RDKB-15468 Unapproved Cisco Copyright Notice (3 of 9)
    Reason for change: Applying Approved Cisco Apache header
    Test Procedure: Build using Jenkins
    Risks: None
    
    Change-Id: Ib26ca0698737c6df928962edb2dd416a68f8be4a
    Signed-off-by: Sachin Kitnawat <Sachin_Kitnawat@comcast.com>

--- scripts/Hotspot/handle_emu_gre.sh
+++ scripts/Hotspot/handle_emu_gre.sh
@@ -34,20 +34,6 @@
 #   limitations under the License.
 #######################################################################
 
-
-#------------------------------------------------------------------
-# Copyright (c) 2013 by Cisco Systems, Inc. All Rights Reserved.
-#
-# This work is subject to U.S. and international copyright laws and
-# treaties. No part of this work may be used, practiced, performed,
-# copied, distributed, revised, modified, translated, abridged, condensed,
-# expanded, collected, compiled, linked, recast, transformed or adapted
-# without the prior written consent of Cisco Systems, Inc. Any use or
-# exploitation of this work without authorization could subject the
-# perpetrator to criminal and civil liability.
-#------------------------------------------------------------------
-
-
 #------------------------------------------------------------------
 # ENTRY
 #------------------------------------------------------------------
